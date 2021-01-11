21ca530236e38feae7d4005b5525194daec3d750
PureGero <puregero@gmail.com>
2021-01-11 23:25:12 +1000

    Disable mob spawns

--- src/main/java/net/justminecraft/minigames/bedwars/BedWars.java
+++ src/main/java/net/justminecraft/minigames/bedwars/BedWars.java
@@ -16,6 +16,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -247,6 +248,13 @@ public class BedWars extends Minigame implements Listener {
         if (MG.core().getGame(e.getEntity().getWorld()) instanceof BedWarsGame) {
             BedWarsGame game = (BedWarsGame) MG.core().getGame(e.getEntity().getWorld());
             e.blockList().removeIf(block -> !game.playerBlocks.contains(block));
+        }
+    }
+
+    @EventHandler
+    public void onCreatureSpawn(CreatureSpawnEvent e) {
+        if (e.getSpawnReason() == CreatureSpawnEvent.SpawnReason.NATURAL) {
+            e.setCancelled(true);
         }
     }
 
